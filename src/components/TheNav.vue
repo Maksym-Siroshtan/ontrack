8c99c7b3033c8099c663a802957951c0ba74bcf7
Maksym <siroshtan.max1991ua@gmail.com>
2023-08-02 21:08:44 +0300

    Modified navigation component, added attribute inheritance

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import NavItems from './NavItems.vue'
 
 const navItems = {
   timeline: ClockIcon,
@@ -11,18 +12,9 @@ const navItems = {
 <template>
   <nav class="sticky bottom-0 z-10 bg-white text-3xl">
     <ul class="flex items-center justify-around border-t">
-      <li
-        v-for="icon, page in navItems"
-        :key="page"
-        class="flex-1"
-      >
-        <a
-          class="flex flex-col items-center p-2 text-xs capitalize"
-          :href="`#${item}`"
-        >
-          <component :is="icon" class="h-6 w-6" /> {{ page }}
-        </a>
-      </li>
+      <NavItems v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+        <component :is="icon" class="h-6 w-6" /> {{ page }}
+      </NavItems>
     </ul>
   </nav>
 </template>

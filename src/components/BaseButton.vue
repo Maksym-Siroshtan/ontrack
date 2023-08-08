3e262bf4e44d47c2b2c845b91f8fdabbe9f3883b
Maksym <siroshtan.max1991ua@gmail.com>
2023-08-08 17:17:50 +0300

    Added BaseButton types and validator

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,7 +1,25 @@
+<script setup>
+import { BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER } from '../constants'
+import { isButtonTypeValid } from '../validators'
+
+const typeClasses = {
+  [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200',
+  [BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white'
+}
+
+defineProps({
+  type: {
+    default: 'neutral',
+    type: String,
+    validator: isButtonTypeValid
+  }
+})
+</script>
+
 <template>
   <button
-      class="rounded bg-gray-100 p-3 enabled:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50"
-    >
-      <slot></slot>
-    </button>
+    :class="`${typeClasses[type]} rounded p-3 disabled:cursor-not-allowed disabled:opacity-50`"
+  >
+    <slot></slot>
+  </button>
 </template>

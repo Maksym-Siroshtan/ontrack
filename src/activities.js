f2632e3ade4037516fa50aa389abce6b2cd8ddfc
Maksym <siroshtan.max1991ua@gmail.com>
2023-08-14 15:14:56 +0300

    Modified activities module

--- src/activities.js
+++ src/activities.js
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 
-import { generateActivities, generateActivitySelectOptions } from './functions'
+import { SECONDS_IN_HOUR } from './constants'
+import { id } from './functions'
 
 export const activities = ref(generateActivities())
 export const activitySelectOptions = computed(() => generateActivitySelectOptions(activities.value))
@@ -15,4 +16,16 @@ export function createActivity(activity) {
 
 export function deleteActivity(activity) {
   activities.value.splice(activities.value.indexOf(activity), 1)
+}
+
+function generateActivities() {
+  return ['Coding', 'Reading', 'Training'].map((name, hours) => ({
+    id: id(),
+    name,
+    secondsToComplete: hours * SECONDS_IN_HOUR
+  }))
+}
+
+function generateActivitySelectOptions(activities) {
+  return activities.map(({ id, name }) => ({ value: id, label: name }))
 }

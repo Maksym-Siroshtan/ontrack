e50685c0e074cbe7bad9deac317f6783fd31d346
Maksym <siroshtan.max1991ua@gmail.com>
2023-08-24 19:01:13 +0300

    Simplifying the implementation of the stopwatch

--- src/composables/stopwatch.js
+++ src/composables/stopwatch.js
@@ -1,31 +1,23 @@
 import { ref } from 'vue'
 import { MILLISECONDS_IN_SECOND } from '../constants'
 
-export function useStopwatch(initialSeconds, handleSecondsChange) {
+export function useStopwatch(initialSeconds) {
   const seconds = ref(initialSeconds)
   const isRunning = ref(false)
   const temp = 120
 
   function start() {
-    isRunning.value = setInterval(() => {
-      seconds.value += temp
-
-      handleSecondsChange()
-    }, MILLISECONDS_IN_SECOND)
+    isRunning.value = setInterval(() => (seconds.value += temp), MILLISECONDS_IN_SECOND)
   }
 
   function stop() {
     clearInterval(isRunning.value)
-
     isRunning.value = false
   }
 
   function reset() {
     stop()
-
     seconds.value = 0
-
-    handleSecondsChange()
   }
 
   return {

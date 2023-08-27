48870dbc27ab757e5ade5d9a778a44a04f5a52fd
Maksym <siroshtan.max1991ua@gmail.com>
2023-08-27 14:13:04 +0300

    Added TheProgressEmptyState component

--- src/components/TheActivitiesEmptyState.vue
+++ src/components/TheActivitiesEmptyState.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col grow items-center gap-4 p-8 text-center">
+  <div class="flex flex-col grow items-center justify-center gap-4 p-8 text-center">
     <img src="../assets/img/no_activities.svg" class="h-48" alt="No activities" />
     <span class="text-gray-400">You don't have any activities</span>
   </div>
